d08da860e62b6ca1c842526265013bb6337ac65c
ashish-manna <m.ashish1920@gmail.com>
2025-09-05 23:27:05 +0530

    user login endpoint done with jwt token

---
+++ src/middlewares/userAuth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+const userAuth = async (req, res, next) => {
+  const { token } = req.cookies;
+  try {
+    if (!token) {
+      throw new Error(`Invalid token`);
+    }
+    const { _id } = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.fineById(_id);
+    if (!user) {
+      throw new Error(`Unauthorized user`);
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(500).json({ message: err.message || `Server Error` });
+  }
+};
+
+module.exports = userAuth;
